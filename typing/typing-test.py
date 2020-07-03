ed80378bfb710de00e74a3daea183186375fc519
Chiwon Cho <goldworm@iconloop.com>
2020-07-04 03:55:37 +0900

    Update typing/typing-test.py

--- typing/typing-test.py
+++ typing/typing-test.py
@@ -1,7 +1,26 @@
 # -*- coding: utf-8 -*-
 
-from typing import get_origin, get_args, List, Dict, Union, Optional, Tuple
+import inspect
+from typing import List, Dict, Union, Optional, Tuple
+from typing import get_origin, get_args, get_type_hints
 from typing_extensions import TypedDict
+
+types = [
+    list,
+    dict,
+    List,
+    List[int],
+    List["int"],
+    Dict,
+    Dict[str, str],
+    Dict[str, "int"],
+    Dict["str", int],
+    Dict["str", "int"],
+    Tuple[int, ...],
+    Optional[int],
+    Union[int, str],
+    Union[bool, Union[int, str]],
+]
 
 
 class Person(TypedDict):
@@ -9,26 +28,7 @@ class Person(TypedDict):
     age: int
 
 
-def handle_typed_dict():
-    print(Person.__annotations__)
-
-
-def main():
-    types = [
-        list,
-        dict,
-        List,
-        List[int],
-        List["int"],
-        Dict,
-        Dict[str, str],
-        Dict[str, "int"],
-        Dict["str", int],
-        Dict["str", "int"],
-        Tuple[int, ...],
-        Optional[int],
-        Union[int, str],
-    ]
+def test_types():
 
     for _type in types:
         print(get_origin(_type))
@@ -36,9 +36,49 @@ def main():
         print("---------------------------")
 
 
-    print(getattr(types, "haha", None))
+def test_types2():
+    print("### test_types2()")
 
-    handle_typed_dict()
+    for _type in types:
+        try:
+            print(_type.__origin__)
+            print(_type.__args__)
+            print("---------------------------")
+        except:
+            pass
+
+
+def test_get_type_hints():
+    def f(a: int = None):
+        pass
+
+    type_hints = get_type_hints(f)
+    print(f"get_type_hints(): {type_hints}")
+
+    sig = inspect.signature(f)
+    parameter = sig.parameters["a"]
+    print(f"inspect: {parameter.annotation}")
+
+
+def handle_typed_dict():
+    print(Person.__annotations__)
+
+
+def test_optional():
+    def func(a: int = None):
+        pass
+
+    sig = inspect.signature(func)
+    print(sig.parameters)
+
+
+def main():
+
+    # test_types()
+    test_types2()
+    # handle_typed_dict()
+    # test_optional()
+    test_get_type_hints()
 
 
 if __name__ == "__main__":
